ba864039c4696e3fdacd13397da786092052a9c8
anamaydev <anamaydev@gmail.com>
2025-07-28 13:08:20 +0530

    refactor: use .env.local for api keys

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import {initializeApp} from "firebase/app";
 import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtaDGREiEyOvVHtKBHpivBHfg6VSRs8F0",
-  authDomain: "quibil.firebaseapp.com",
-  projectId: "quibil",
-  storageBucket: "quibil.firebasestorage.app",
-  messagingSenderId: "182347198135",
-  appId: "1:182347198135:web:642f97e87ef1fe371b6732",
-  measurementId: "G-6QR3YWTGYW"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
