ced234be836944417466be3a3a8641a01098adc5
anamaydev <anamaydev@gmail.com>
2025-08-02 15:18:33 +0530

    feat: fetch tenants data from firestore

--- src/context/Tenant/TenantProvider.tsx
+++ src/context/Tenant/TenantProvider.tsx
@@ -1,6 +1,6 @@
-import {useState} from "react";
+import {useState, useEffect, useMemo} from "react";
 import {TenantContext, type Tenant} from "./TenantContext";
-import {addTenant} from "@/services/tenants.ts";
+import {addTenant, getTenants} from "@/services/tenants.ts";
 import {Outlet} from "react-router-dom";
 
 const TenantProvider = () => {
@@ -14,14 +14,36 @@ const TenantProvider = () => {
     ownerId: ""
   })
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [tenants, _setTenants] = useState<Tenant[]>([]);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [loading, _setLoading] = useState(true);
+  const [tenants, setTenants] = useState<Tenant[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const cached = sessionStorage.getItem("tenantsData");
+    if(cached) {
+      setTenants(JSON.parse(cached));
+      setLoading(false);
+    }
+    else{
+      async function handleGetTenants(){
+        const snapshot = await getTenants();
+        const tenantsData = snapshot.docs.map(doc => (
+          {id: doc.id, ...doc.data()}
+        ))
+
+        setTenants(tenantsData);
+        setLoading(false);
+        sessionStorage.setItem("tenantsData", JSON.stringify(tenantsData));
+      }
+
+      handleGetTenants();
+    }
+  }, []);
+
+  useEffect(()=>console.log("tenants: ", tenants), [tenants]);
 
   return (
     <TenantContext.Provider value={{tenant, setTenant, tenants, loading, addTenant}}>
-      {/*{children}*/}
+      {/*{loading ? <h1>Loading...</h1> : <Outlet/>}*/}
       <Outlet/>
     </TenantContext.Provider>
   )
--- src/services/tenants.ts
+++ src/services/tenants.ts
@@ -1,9 +1,13 @@
 import {db} from "@/lib/firebase";
-import {collection, addDoc} from "firebase/firestore";
+import {collection, addDoc, getDocs} from "firebase/firestore";
 import {type Tenant} from "@/context/Tenant/TenantContext";
 
 const tenantsRef = collection(db, "tenants");
 
 export function addTenant(tenantData: Tenant){
   return addDoc(tenantsRef, tenantData);
+}
+
+export function getTenants(){
+  return getDocs(tenantsRef);
 }
